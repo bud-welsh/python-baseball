8909a57b92c638deabf5a0ce946d8b6eba422259
Bud Welsh <budwelsh14@gmail.com>
2020-01-19 13:41:48 -0600

    Minor edits to comments.

--- retrosheet-downloads/download-retrosheet-events.py
+++ retrosheet-downloads/download-retrosheet-events.py
@@ -1,13 +1,12 @@
-# This will download all the event files from 1918 to 2019 from retrosheet.org
-# 19 January 2020
+# This will download all the event files from 1918 to 2019 from retrosheet.org.
 
-# https://www.retrosheet.org/game.htm has the event file links
+# https://www.retrosheet.org/game.htm has the event file links.
 
-# These are the paths to the yearly event files on retrosheet.org
+# These are the paths to the yearly event files on retrosheet.org:
 # https://www.retrosheet.org/events/1918eve.zip
 # https://www.retrosheet.org/events/1919eve.zip
 
-# Found urllib.request module from an article, https://stackabuse.com/download-files-with-python/, and the python documents, https://docs.python.org/3/library/urllib.request.html#module-urllib.request
+# Found urllib.request module from an article, https://stackabuse.com/download-files-with-python/, and the python documents, https://docs.python.org/3/library/urllib.request.html#module-urllib.request.
 
 import urllib.request 
 
@@ -18,7 +17,7 @@ print("Starting to download event files")
 while year_download < 2020:
     url = "https://www.retrosheet.org/events/" + str(year_download) + "eve.zip"
     urllib.request.urlretrieve(url, '/home/tk421/retrosheet/regular-season-events/' + str(year_download) + 'eve.zip')
-    print("Downloading " + str(year_download) + " event files") # Don't need this step, but it is nice to see it anyway"
+    print("Downloading " + str(year_download) + " event files") # I don't need this step, but it is nice to see it anyway because of the amount of time it takes to download each file.
     year_download += 1
 
 print("Event files download complete")
