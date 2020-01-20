44a2debe6c29236e8a7ee9aa4bdee691afa7bcaa
Bud Welsh <budwelsh14@gmail.com>
2020-01-20 12:00:46 -0600

    Add zip-files to each directory

--- retrosheet-downloads/creating-directories.py
+++ retrosheet-downloads/creating-directories.py
@@ -6,32 +6,32 @@ import os
 print("The path to where I want to put my retrosheet folder is: /home/tk421/")
 retrosheet_home = input("What is the path to where you want to put the retrosheet folder? ")
 
-regular_events_path = retrosheet_home + 'retrosheet/regular-season-events' 
-regular_box_path = retrosheet_home + 'retrosheet/regular-box-score'
-allstar_events_path = retrosheet_home + 'retrosheet/all-star-events'
-postseason_events_path = retrosheet_home + 'retrosheet/post-season-events'
+regular_events_path = retrosheet_home + 'retrosheet/regular-season-events/zip-files' 
+regular_box_path = retrosheet_home + 'retrosheet/regular-box-score/zip-files'
+allstar_events_path = retrosheet_home + 'retrosheet/all-star-events/zip-files'
+postseason_events_path = retrosheet_home + 'retrosheet/post-season-events/zip-files'
 disrepancy_path = retrosheet_home + 'retrosheet/discrepancy-files'
-regular_gamelog_path = retrosheet_home + 'retrosheet/regular-season-game-logs'
-world_series_gamelog_path = retrosheet_home + 'retrosheet/world-series-game-logs'
-allstar_gamelog_path = retrosheet_home + 'retrosheet/all-star-game-logs'
-wildcard_gamelog_path = retrosheet_home + 'retrosheet/wild-card-game-logs'
-lds_gamelog_path = retrosheet_home + 'retrosheet/league-divisional-series-game-logs'
-lcs_gamelog_path = retrosheet_home + 'retrosheet/league-championship-series-game-logs'
-regular_schedule_path = retrosheet_home + 'retrosheet/regular-season-schedules'
+regular_gamelog_path = retrosheet_home + 'retrosheet/regular-season-game-logs/zip-files'
+world_series_gamelog_path = retrosheet_home + 'retrosheet/world-series-game-logs/zip-files'
+allstar_gamelog_path = retrosheet_home + 'retrosheet/all-star-game-logs/zip-files'
+wildcard_gamelog_path = retrosheet_home + 'retrosheet/wild-card-game-logs/zip-files'
+lds_gamelog_path = retrosheet_home + 'retrosheet/league-divisional-series-game-logs/zip-files'
+lcs_gamelog_path = retrosheet_home + 'retrosheet/league-championship-series-game-logs/zip-files'
+regular_schedule_path = retrosheet_home + 'retrosheet/regular-season-schedules/zip-files'
 
 try:
-    os.makedirs(regular_events_path) # This one creates retrosheet directory and regular-seaon-events directory inside the retrosheet directory at the same time
-    os.mkdir(regular_box_path)
-    os.mkdir(allstar_events_path)
-    os.mkdir(postseason_events_path)
-    os.mkdir(disrepancy_path)
-    os.mkdir(regular_gamelog_path)
-    os.mkdir(world_series_gamelog_path)
-    os.mkdir(allstar_gamelog_path)
-    os.mkdir(wildcard_gamelog_path)
-    os.mkdir(lds_gamelog_path)
-    os.mkdir(lcs_gamelog_path)
-    os.mkdir(regular_schedule_path)
+    os.makedirs(regular_events_path) 
+    os.makedirs(regular_box_path)
+    os.makedirs(allstar_events_path)
+    os.makedirs(postseason_events_path)
+    os.makedirs(disrepancy_path)
+    os.makedirs(regular_gamelog_path)
+    os.makedirs(world_series_gamelog_path)
+    os.makedirs(allstar_gamelog_path)
+    os.makedirs(wildcard_gamelog_path)
+    os.makedirs(lds_gamelog_path)
+    os.makedirs(lcs_gamelog_path)
+    os.makedirs(regular_schedule_path)
 except OSError:
     print("Creation of directory %s failed" % regular_events_path)
     print("Creation of directory %s failed" % regular_box_path)
