539d37c2a3b8b015b2dfe5577b83b4ad27252347
Bud Welsh <budwelsh14@gmail.com>
2020-01-21 20:17:11 -0600

    Add batting variables.

--- baseball-model-v1.py
+++ baseball-model-v1.py
@@ -1,5 +1,20 @@
-# First half inning
+# Here are the batting variables.
+plate_apperances = 186518
+at_bats = 166651
+hits = 42039
+doubles = 8531
+triples = 785
+home_runs = 6776
+singles = (42039 - doubles - triples - home_runs)
+walks = 15895
+strike_outs = 42823
+hit_by_pitch = 1984
+sacrifice_hits = 776
+sacrifice_flies = 1150
 
+
+
+# The first half inning
 for outs in range(0, 4):
     if outs < 3:
         print("Batter up!")
