3d4b0076fe90ad01e654744878c17c4e12203e20
Bud Welsh <budwelsh14@gmail.com>
2020-01-22 05:53:46 -0600

    Rename baseball model

--- baseball_model_v1.py
+++ baseball_model_v1.py
@@ -1,3 +1,5 @@
+
+
 # Here are the 2019 batting variables.
 plate_apperances = 186518
 at_bats = 166651
@@ -11,7 +13,7 @@ strike_outs = 42823
 hit_by_pitch = 1984
 outs = (plate_apperances - hits - walks - hit_by_pitch - strike_outs)
 
-# These are the 2019 random batting percentages
+# These are the 2019 random batting percentages.
 single_average = singles / plate_apperances
 double_average = doubles / plate_apperances
 triple_average = triples / plate_apperances
@@ -23,11 +25,5 @@ out_average = outs / plate_apperances
 
 print(single_average + double_average + triple_average + home_run_average + walk_average + strike_out_average + hit_by_pitch_average + out_average)
 
-# The first half inning
-for outs in range(0, 4):
-    if outs < 3:
-        print("Batter up!")
-        print("Batter strikes out.")
-        outs += 1
-    else:
-        print("That's the end of the half inning.")
+# This will decide the play outcome.
+
