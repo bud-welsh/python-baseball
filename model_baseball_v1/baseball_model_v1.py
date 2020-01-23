c2b3ced789cb0bc9cfe7c4f46ad37086f2df3ccf
Bud Welsh <budwelsh14@gmail.com>
2020-01-22 19:40:07 -0600

    Add test for single probability and gitignore file

--- model_baseball_v1/baseball_model_v1.py
+++ model_baseball_v1/baseball_model_v1.py
@@ -7,27 +7,23 @@ hits_raw = 42039
 doubles_raw = 8531
 triples_raw = 785
 home_runs_raw = 6776
-singles_raw = (42039 - doubles - triples - home_runs)
+singles_raw = (42039 - doubles_raw - triples_raw - home_runs_raw)
 walks_raw = 15895
 strike_outs_raw = 42823
 hit_by_pitch_raw = 1984
 outs_raw = (plate_apperances_raw - hits_raw - walks_raw - hit_by_pitch_raw - strike_outs_raw)
 
 # These are the 2019 random batting percentages.
-single_probability = singles_raw / plate_apperances_raw
-double_probability = doubles_raw / plate_apperances_raw
-triple_probability = triples_raw / plate_apperances_raw
-home_run_probability = home_runs_raw / plate_apperances_raw
-walk_probability = walks_raw / plate_apperances_raw
-strike_out_probability = strike_outs_raw / plate_apperances_raw
-hit_by_pitch_probability = hit_by_pitch_raw / plate_apperances_raw
-out_probability = outs_raw / plate_apperances_raw
+def single_probability(singles_raw, plate_apperances_raw):
+    return singles_raw / plate_apperances_raw
 
-print(single_probability + double_probability + triple_probability + home_run_probability + walk_probability + strike_out_probability + hit_by_pitch_probability + out_probability)
+# single_probability = singles_raw / plate_apperances_raw
+# double_probability = doubles_raw / plate_apperances_raw
+# triple_probability = triples_raw / plate_apperances_raw
+# home_run_probability = home_runs_raw / plate_apperances_raw
+# walk_probability = walks_raw / plate_apperances_raw
+# strike_out_probability = strike_outs_raw / plate_apperances_raw
+# hit_by_pitch_probability = hit_by_pitch_raw / plate_apperances_raw
+# out_probability = outs_raw / plate_apperances_raw
 
-# This will decide the play outcome.
-
-# This will count the play outcome result
-def count_play_result(play_outcome):
-    if batted_single:
-        hit = hit + 1
+# print(single_probability + double_probability + triple_probability + home_run_probability + walk_probability + strike_out_probability + hit_by_pitch_probability + out_probability)
--- model_baseball_v1/test_baseball_model.py
+++ model_baseball_v1/test_baseball_model.py
@@ -1,5 +1,9 @@
 import baseball_model_v1
 import unittest
 
-class TestCountPlayResult(unittest.TestCase):
-    def test_count_play_result(self):
+class Test_GetProbability(unittest.TestCase):
+    def test_single_probability(self):
+        self.assertEqual(baseball_model_v1.single_probability(1, 3), 0.3333333333333333)
+
+if __name__ == '__main__':
+    unittest.main()
