a5cfede1b4eb60c475bcb73c985b966db4e148a8
Bud Welsh <budwelsh14@gmail.com>
2020-01-23 20:33:15 -0600

    Delete some comments

--- model_baseball_v1/baseball_model_v1.py
+++ model_baseball_v1/baseball_model_v1.py
@@ -38,13 +38,3 @@ def hit_by_pitch_probability(hit_by_pitch_raw, plate_apperances_raw):
 def out_probability(outs_raw, plate_apperances_raw):
     return outs_raw / plate_apperances_raw
 
-# single_probability = singles_raw / plate_apperances_raw
-# double_probability = doubles_raw / plate_apperances_raw
-# triple_probability = triples_raw / plate_apperances_raw
-# home_run_probability = home_runs_raw / plate_apperances_raw
-# walk_probability = walks_raw / plate_apperances_raw
-# strike_out_probability = strike_outs_raw / plate_apperances_raw
-# hit_by_pitch_probability = hit_by_pitch_raw / plate_apperances_raw
-# out_probability = outs_raw / plate_apperances_raw
-
-# print(single_probability + double_probability + triple_probability + home_run_probability + walk_probability + strike_out_probability + hit_by_pitch_probability + out_probability)
